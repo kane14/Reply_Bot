b055c63b184139fd28d38895391828aa157d6689
kane14 <momenteye2141@gmail.com>
2016-10-08 13:28:08 +0000

    first commit

---
+++ reply_bot.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import tweepy
+from datetime import timedelta
+import datetime
+
+CK="XXXXXXXXXXXXXXXXXXXXXXXX"
+CS="XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+AT="XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+AS="XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+
+# Twitterオブジェクトの生成
+auth = tweepy.OAuthHandler(CK, CS)
+auth.set_access_token(AT, AS)
+
+api = tweepy.API(auth)
+
+class Listener(tweepy.StreamListener):
+    def on_status(self, status):
+        status.created_at += timedelta(hours=9)#世界標準時から日本時間に
+
+	# リプライが来たら返信
+        print(status.text)
+	print(str(status.in_reply_to_screen_name))
+	print(str(status.user.screen_name))
+	if str(status.in_reply_to_screen_name)=="{TWITTER_ID}" and str(status.user.screen_name)=="momenteye":
+		tweet = "@" + str(status.user.screen_name) + " " + "Yeah!\n" \
+			+ str(datetime.datetime.today())
+		api.update_status(status=tweet)
+	return True
+     
+    def on_error(self, status_code):
+        print('Got an error with status code: ' + str(status_code))
+        return True
+     
+    def on_timeout(self):
+        print('Timeout...')
+        return True
+ 
+# Twitterオブジェクトの生成
+auth = tweepy.OAuthHandler(CK, CS)
+auth.set_access_token(AT, AS)
+ 
+listener = Listener()
+stream = tweepy.Stream(auth, listener)
+stream.userstream()
